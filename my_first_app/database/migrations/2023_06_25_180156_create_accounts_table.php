656d601d4bc13c6e401daea2b62c5a74fc4c7af3
RenmarB <renmarfrancia.balana@bicol-u.edu.ph>
2023-06-26 03:06:43 +0800

    Create 2023_06_25_180156_create_accounts_table.php

---
+++ my_first_app/database/migrations/2023_06_25_180156_create_accounts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('account_type', ['admin', 'student'])->default('student');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
